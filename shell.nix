7d905a021ccc8ae85aca9f378deedff9fcd8a493
Milan Hauth <milahu@gmail.com>
2024-01-03 15:05:47 +0100

    add links

--- shell.nix
+++ shell.nix
@@ -7,7 +7,9 @@ mkShell {
   buildInputs = [
     #apt
     #dpkg
+    # https://github.com/milahu/nur-packages/tree/master/pkgs/tools/package-management/apt-file
     nur.repos.milahu.apt-file
+    # https://github.com/milahu/nur-packages/tree/master/pkgs/tools/package-management/apt-init-config
     nur.repos.milahu.apt-init-config
   ];
 
